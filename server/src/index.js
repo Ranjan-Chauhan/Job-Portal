34f71ce5236439c49e25e84fa88abbe439196521
Ranjan Chauhan <chauhanranjan8935@gmail.com>
2025-04-25 17:14:21 +0530

    adding cors

--- server/src/index.js
+++ server/src/index.js
@@ -11,14 +11,14 @@ import applicationRoutes from "./routes/application.routes.js";
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173/*",
+    credentials: true,
+  })
+);
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
