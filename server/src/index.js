93014328e896da529af0c19ccf626e61e448e392
Ranjan Chauhan <chauhanranjan8935@gmail.com>
2025-04-25 17:09:35 +0530

    adding cors

--- server/src/index.js
+++ server/src/index.js
@@ -11,13 +11,14 @@ import applicationRoutes from "./routes/application.routes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://job-portal-877n.onrender.com",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
